394a1b6d4ef8610c98000f57717b815997720d52
Dipayan Das <ddipayan00@gmail.com>
2024-02-29 10:21:27 +0530

    more changes

---
+++ code_collection_module/testCode.py
@@ -0,0 +1,21 @@
+from context_manager_practices.code import main as context_manager_code 
+from collections_practices.code import main as collections_code
+from decorators_practices.code import main as decorators_code
+from dictionaries_practices.code import main as dictionaries_code
+from exceptions_practices.code import main as exceptions_code
+from generators_practices.code import main as generators_code
+from itertools_practices.code import main as itertools_code
+from json_practices.code import main as json_code
+from set_practices.code import main as set_code
+from shallow_and_deep_copy_practices.code import main as shallow_and_deep_copy_practices_code
+from logging_practices.code import main as logging_code
+from threading_practices.code import main as threading_code
+from string_practices.code import main as string_code
+from multiprocessing_practices.code import main as multiprocessing_code
+from list_practices.code import main as list_code
+from python_lambda_practices.code import main as python_lambda_code
+from random_number_practices.code import main as random_number_code
+from tuple_practices.code import main as tuple_practices_code
+
+if __name__ == "__main__":
+    tuple_practices_code()
